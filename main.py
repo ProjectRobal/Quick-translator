0cd4729d9fc8727613fe0baa38565c4b5f808f78
ProjectRobal <46270898+ProjectRobal@users.noreply.github.com>
2022-10-27 16:55:20 +0200

    The backend has been written, now I have to test it

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from scipy.io.wavfile import write
 import numpy
 import ctypes
 import os
+from tts_interface import TTSInterface
 
 # config variables
 from config import *
@@ -30,6 +31,81 @@ class AudioSamp:
     @property
     def buffer(self):
         return self._buffer
+
+class TranslatorInput:
+    def __init__(self,input_text:str,input_lang:str,output_lang:str,audio_samp:AudioSamp):
+        self._input_lang = input_lang
+        self._output_lang = output_lang
+        self._input_text = input_text
+        self._audio_samp=audio_samp
+
+    def audio_samp(self):
+        return self._audio_samp
+
+    @property
+    def input_lang(self):
+        return self._input_lang
+
+    @property
+    def output_lang(self):
+        return self._output_lang
+
+    @property
+    def text(self):
+        return self._input_text
+
+class TTSInput:
+    def __init__(self,input_text:str,output_lang:str,audio_samp:AudioSamp):
+        self._output_lang = output_lang
+        self._input_text = input_text
+        self._audio_samp= audio_samp
+
+    def audio_samp(self):
+        return self._audio_samp
+
+    @property
+    def output_lang(self):
+        return self._output_lang
+
+    @property
+    def text(self):
+        return self._input_text
+
+
+class Speaker:
+
+    def __init__(self,chunk,format=pyaudio.paInt16,channels=1,rate=16000,id=0):
+
+        self.audio=pyaudio.PyAudio()
+        self.open_stream(format,channels,rate,chunk,id)
+
+    def open_stream(self,format,channels,rate,chunk,id=0):
+
+        self.close_stream()
+
+        self.format = format
+        self.channels = channels
+        self.rate = rate
+        self.chunk = chunk
+        self.id = id
+
+        self.stream=self.audio.open(format=format, channels=channels,rate=rate,frames_per_buffer=chunk,output=True,output_device_index=id)
+        self.chunk=chunk
+
+    def put(self,sample:numpy.array):
+        self.stream.write(sample,len(sample),exception_on_overflow = False)
+
+    '''Close current stream'''
+    def close_stream(self):
+        if hasattr(self,'stream'):
+            self.stream.stop_stream()
+            self.stream.close()
+
+    '''Close speaker completely'''
+    def close(self):
+        self.close_stream()
+        self.audio.terminate()
+        
 
 
 
@@ -65,7 +141,7 @@ class Microphone:
 
     '''Get a chunk then check if it has a voice , if it is true it return a array with chunk else it returns None '''
     def get(self):
-        frame=self.stream.read(CHUNK,exception_on_overflow = False)
+        frame=self.stream.read(self.chunk,exception_on_overflow = False)
 
         if self.vad.is_speech(frame,self.rate):
             return numpy.frombuffer(frame,dtype=numpy.int16)
@@ -88,16 +164,26 @@ class Microphone:
 
 
 class QuickTrans:
-    def __init__(self,mic:Microphone,langs):
+    def __init__(self,mic:Microphone,speaker:Speaker,langs):
         self._langs = langs
         self._input_lang=self.check_input_langs()
         self._output_lang=self.check_output_langs()
         self._mic = mic
-        self.stt_queue = queue.Queue(20)
+
+        self.stt_queue = queue.Queue(20) #a queue for stt thread
         self.stt_th=None
+
+        self.tts_queue = queue.Queue(20) #a queue for tts thread
+        self.tts_th=None
+
+        self.trans_queue = queue.Queue(20) #a queue for translator thread
+        self.trans_th=None
+
         self.clear_buffer()
         self.speech_begin=False
         self.recording=False
+
+        self._speaker=speaker
 
     def check_input_langs(self):
         entries=os.listdir(STT_DIRECTORY+"/")
@@ -162,10 +248,21 @@ class QuickTrans:
 
         self.stt_th.start()
 
+        self.trans_th=threading.Thread(target=self.trans_thread)
+
+        self.trans_th.start()
+
+        self.tts_th=threading.Thread(target=self.tts_thread)
+
+        self.tts_th.start()
+
     def close_threads(self):
         '''close stt and tts threads'''
         ctypes.pythonapi.PyThreadState_SetAsyncExc(self.stt_th.ident,ctypes.py_object(SystemExit))
 
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(self.tts_th.ident,ctypes.py_object(SystemExit))
+
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(self.trans_th.ident,ctypes.py_object(SystemExit))
 
     def speech_to_text(self,audio,input_lang:str)->str:
         '''perform stt , audio - is a input audio data (numpy.array int16) , input_lang - a source language'''
@@ -197,9 +294,8 @@ class QuickTrans:
                 print(output)
                 '''check if stt generated valid output'''
                 if type(output)==str and len(output)>0:
-                    '''then translate it'''
-                    translated=self.translate(output,item.input_lang,item.output_lang)
-                    print(translated.text)
+                    '''then translate it'''                    
+                    self.put_to_translator(TranslatorInput(output,item.input_lang,item.output_lang,item))
 
             self.stt_queue.task_done()
 
@@ -207,9 +303,63 @@ class QuickTrans:
         '''put audio sample to stt thread'''
         self.stt_queue.put(smp)
 
+    def trans_thread(self):
+        '''google translate thread'''
+
+        while True:
+            item:TranslatorInput=self.trans_queue.get()
+
+            if len(item.text)>0:
+                    '''then translate it'''
+                    translated=self.translate(item.text,item.input_lang,item.output_lang)
+                    print(translated.text)
+
+                    self.put_to_tts(TTSInput(translated,item.input_lang,item.output_lang,item.audio_samp()))
+            
+            self.trans_queue.task_done()
+
+
+    def put_to_translator(self,item:TranslatorInput):
+        self.trans_queue.put(item)
+
+    def tts_thread(self):
+        '''tts thread function'''
+        while True:
+            item:TTSInput=self.tts_queue.get()
+
+            text=item.text
+
+            output_lang=item.output_lang
+
+            samp=item.audio_samp().buffer
+
+            # cut it off
+            if len(samp)> 16000*5:
+                samp=samp[0:16000*5]
+
+            try:
+                
+                interface=TTSInterface(TTS_DIRECTORY+"/"+output_lang+"/")
+
+                (wav,alignment)=interface.do_tts(text,samp)
+
+                self._speaker.put(wav)
+
+            except Exception as e:
+                print(e)
+
+            self.tts_queue.task_done()
+
+    def put_to_tts(self,item:TTSInput):
+        self.tts_queue.put(item)
+
     def mic(self):
         '''get microphone object'''
         return self._mic
+
+    def speaker(self):
+        '''get speaker object'''
+        return self._speaker
 
     def startRecording(self):
         self.speech_begin=True
--- tts_interface.py
+++ tts_interface.py
@@ -14,12 +14,14 @@ from TTS.tts.models.vits import *
 
 from config import *
 
+'''
 MODEL_PATH = 'best_model.pth.tar'
 CONFIG_PATH = 'config.json'
 TTS_LANGUAGES = "language_ids.json"
 TTS_SPEAKERS = "speakers.json"
+'''
 
-class TTSInterface():
+class TTSInterface:
 
     def __init__(self,directory):
         self._directory = directory
@@ -31,15 +33,15 @@ class TTSInterface():
         self.C = load_config(self._directory+"/config.json")
 
         # load the audio processor
-        self.ap = AudioProcessor(**C.audio)
+        self.ap = AudioProcessor(**self.C.audio)
 
         self.speaker_embedding = None
 
         self.C.model_args['d_vector_file'] = self._directory+"/speakers.json"
         self.C.model_args['use_speaker_encoder_as_loss'] = False
 
-        model = setup_model(C)
-        model.language_manager.set_language_ids_from_file(self._directory+"/language_ids.json")
+        self.model = setup_model(self.C)
+        self.model.language_manager.set_language_ids_from_file(self._directory+"/language_ids.json")
         # print(model.language_manager.num_languages, model.embedded_language_dim)
         # print(model.emb_l)
         cp = torch.load(MODEL_PATH, map_location=torch.device('cpu'))
@@ -49,18 +51,45 @@ class TTSInterface():
             if "speaker_encoder" in key:
                 del model_weights[key]
 
-        model.load_state_dict(model_weights)
+        self.model.load_state_dict(model_weights)
 
-        model.eval()
+        self.model.eval()
 
         if USE_CUDA:
             model = model.cuda()
 
-        # synthesize voice
-        self.use_griffin_lim = False
+        self.model.length_scale=1
+        self.model.inference_noise_scale=0.3
+        self.model.inference_noise_scale_dp=0.3
+
+        #self.use_griffin_lim = False
         self.speaker_manager= SpeakerManager(encoder_model_path=self._directory+"/SE_checkpoint.pth", encoder_config_path=self._directory+"/config_se.json",use_cuda=USE_CUDA)
 
-    def compute_spec(self,y,samp=16000):
-        spec=self.ap.spectogram(y,samp)
-        spec = torch.FloatTensor(spec).unsqueeze(0)
-        return spec
+    def compute_spec(self,y):
+        return self.ap.melspectograms(y)
+
+    def compute_d_vector(self,spec):
+        return self.speaker_manager.compute_embeddings(spec.T)
+
+    def do_tts(self,text,sample):
+        d_vector=self.compute_d_vector(self.compute_spec(sample))
+        language_id=0
+
+        wav, alignment, _, _ = synthesis(
+                    self.model,
+                    text,
+                    self.C,
+                    "cuda" in str(next(self.model.parameters()).device),
+                    self.ap,
+                    speaker_id=None,
+                    d_vector=d_vector,
+                    style_wav=None,
+                    language_id=language_id,
+                    enable_eos_bos_chars=self.C.enable_eos_bos_chars,
+                    use_griffin_lim=True,
+                    do_trim_silence=False,
+                ).values()
+
+        return (wav,alignment)
+
+        
